1acf3fc6dca273e917ccfb54d9a0939b4636db99
Dekillston <75752521+Dekillston@users.noreply.github.com>
2023-06-13 19:54:06 +0300

    jdex.js

--- jdex.js
+++ jdex.js
@@ -22,24 +22,3 @@ function IdSettings(bite) {
 }
 Object.prototype.ID = ID;
 Object.prototype.IDall = IDall;
-//
-//
-//
-//
-function AJAX(file, fun) {
-	var data = this[0];
-	try {
-		$.ajax({
-	        type: "POST",
-	        url: file,
-	        dataType: "json",
-	        data: data,
-	        success: function(d) {
-	            fun(d);
-	        }
-	    });
-	} catch (err) {
-		console.log('%c Error: Non connection jQuery!', 'background: rgb(122, 0, 0, 0.63); color: #ff8c8c');
-	}
-}
-Array.prototype.AJAX = AJAX;
